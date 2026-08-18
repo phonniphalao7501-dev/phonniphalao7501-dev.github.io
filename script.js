4c27d7b38b803a8231ea42d2d42d3038ad45cfef
phonniphalao7501-dev <phonnipha.lao7501@ndlp.go.th>
2026-08-18 11:42:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
 document.addEventListener('DOMContentLoaded',function () {
     document.getElementById('year').innerHTML = new Date().getFullYear();
     console.log('Portfolio ของ Phornnipha พร้อมแล้ว!');
